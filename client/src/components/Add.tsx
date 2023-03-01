ffc852bf9d79d433c0d2ac72ab256857b424b02a
Andrew Chaa <andrew.yh.chaa@gmail.com>
2023-03-01 23:58:05 +0000

    button

--- client/src/components/Add.tsx
+++ client/src/components/Add.tsx
@@ -42,14 +42,15 @@ export default function Add({
         Your password must be less than 4 characters.
       </p>
 
-      <button
-        onClick={handleAdd}
-        type="button"
-        className="inline-flex items-center rounded border border-gray-300 bg-white px-2.5 py-1.5 text-xs font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-      >
-        Add
-      </button>
+      <div className="mt-5">
+        <button
+          onClick={handleAdd}
+          type="button"
+          className="inline-flex items-center rounded border border-gray-300 bg-white px-2.5 py-1.5 text-xs font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+        >
+          Add
+        </button>
+      </div>
     </div>
-
   );
 }
